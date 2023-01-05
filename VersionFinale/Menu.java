5768730fe4a38ddd1852e8a2a4e25e44a07abb8c
Baptiste-B14 <97801100+Baptiste-B14@users.noreply.github.com>
2023-01-05 14:58:59 +0100

    Update Menu.java

--- VersionFinale/Menu.java
+++ VersionFinale/Menu.java
@@ -7,11 +7,11 @@ public class Menu {
         Scanner sc = new Scanner(System.in).useDelimiter("\n");
         //Declaration et initialisation des tableaux de positionnement
 
-        //int[][] tblanc = {{0,4},{0,3},{0,2},{0,5},{0,1},{0,6},{0,0},{0,7},{1,0},{1,1},{1,2},{1,3},{1,4},{1,5},{1,6},{1,7}};
-        //int[][] tnoir = {{7,4},{7,3},{7,2},{7,5},{7,1},{7,6},{7,0},{7,7},{6,0},{6,1},{6,2},{6,3},{6,4},{6,5},{6,6},{6,7}};
+        int[][] tblanc = {{0,4},{0,3},{0,2},{0,5},{0,1},{0,6},{0,0},{0,7},{1,0},{1,1},{1,2},{1,3},{1,4},{1,5},{1,6},{1,7}};
+        int[][] tnoir = {{7,4},{7,3},{7,2},{7,5},{7,1},{7,6},{7,0},{7,7},{6,0},{6,1},{6,2},{6,3},{6,4},{6,5},{6,6},{6,7}};
 
-        int[][] tblanc = {{0,7},{-1,-3},{2,5},{-1,-5},{7,4},{-1,-6},{0,1},{3,0},{3,3},{-1,-1},{-1,-2},{-1,-3},{-1,-4},{-1,-5},{-1,-6},{-1,-7}};
-        int[][] tnoir = {{5,1},{-7,-3},{-7,-2},{-7,-5},{-7,-1},{-7,-6},{-7,-0},{-7,-7},{-6,-0},{-6,-1},{-6,-2},{-6,-3},{-6,-4},{-6,-5},{-6,-6},{-6,-7}};
+        //int[][] tblanc = {{0,7},{-1,-3},{2,5},{-1,-5},{7,4},{-1,-6},{0,1},{3,0},{3,3},{-1,-1},{-1,-2},{-1,-3},{-1,-4},{-1,-5},{-1,-6},{-1,-7}};
+        //int[][] tnoir = {{5,1},{-7,-3},{-7,-2},{-7,-5},{-7,-1},{-7,-6},{-7,-1},{-7,-7},{-6, -1},{-6,-1},{-6,-2},{-6,-3},{-6,-4},{-6,-5},{-6,-6},{-6,-7}};
 
         int couleur; //blanc =0; noir = 1
         String sCouleur;
@@ -23,8 +23,8 @@ public class Menu {
         int piece;
         int[] position = new int[2];
 
+        Affichage.printBoard(tblanc, tnoir);
 
-        //boucle de partie (on en sort lorsque échecEtMat)
         do {
 
 
@@ -32,18 +32,15 @@ public class Menu {
             if (couleur == 1) sCouleur = "noir";
             else sCouleur = "blanc";
 
-            System.out.println("Tour n°" + tour + " Joueur : " + sCouleur);
-            
-            //boucle de chaque tour (saisie valide)
+            System.out.println("Tour n°" + (tour+1) + " Joueur : " + sCouleur);
+
             do {
                 tabJoueur = new int[16][2];
-                tabEnnemi= new int[16][2];
-
-
+                tabEnnemi = new int[16][2];
 
                 //Joueur en cours.
                 if (couleur == 0) {//copie par for
-                    //On fait correspondre le tableau du joueur en cours à sa couleur
+
                     for (int i = 0; i < tblanc.length; i++) {
                         for (int j = 0; j < tblanc[i].length; j++) {
                             tabJoueur[i][j] = tblanc[i][j];
@@ -60,9 +57,9 @@ public class Menu {
                     }
 
                 }
-                //Dans cette boucle, on demande le coup du joueur et on vérifie qu'il soit faisable
+
                 do {
-            
+
 
                     do {
 
@@ -72,6 +69,7 @@ public class Menu {
                             piece = Fonction.demandeSaisie(tabJoueur);
 
                             //Demande la future position de la pièce.
+
                             position = Fonction.demandePosition();
 
 
@@ -88,8 +86,6 @@ public class Menu {
                     System.out.println("schema de deplacement valide");
                 } while (!Fonction.verificationDuChemin(piece, position[0], position[1], tabJoueur, tabEnnemi));
                 System.out.println("pas d'obstacle");
-                //Une fois avoir vérifié que le coup est possible, on actualise l'échiquier qui n'est toujours pas permanent
-                // pour voir si le roi du joueur est mis en échec.
                 Fonction.actualisationEchiquier(position[0], position[1], piece, tabJoueur, tabEnnemi);
 
 
@@ -97,7 +93,6 @@ public class Menu {
 
 
             //Joueur en cours.
-            //Après avoir actualisé l'échiquier avec le nouveau coup du joueur, on rend cet échiquier permanent.
             if (couleur == 0) {
                 tblanc = tabJoueur;
                 tnoir = tabEnnemi;
@@ -105,11 +100,11 @@ public class Menu {
                 tnoir = tabJoueur;
                 tblanc = tabEnnemi;
             }
-
+            Affichage.printBoard(tblanc, tnoir);
 
             //affichage()
 
-            // On teste d'abord si le roi adverse est en échec, puis on vérifie s'il est mis en échec et Mat
+
             if (Fonction.echecRoi(couleur, tabEnnemi, tabJoueur)) {//Ne pas modifier l'ordre des tab param. On cherche le roi ennemi (fin du tour)
                 echecEtMat = Fonction.echecEtMat(tblanc, tnoir, couleur, tour);
             }
