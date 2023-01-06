5477ccc8d9fb09bc5d685eaf836bb96bc39c0d85
Baptiste-B14 <97801100+Baptiste-B14@users.noreply.github.com>
2023-01-06 02:17:18 +0100

    Update Affichage.java

--- VersionFinale/Affichage.java
+++ VersionFinale/Affichage.java
@@ -1,19 +1,19 @@
 
 public class Affichage {
 
-    public static void printBoard(int[][]tabBlanc, int[][] tabNoir){
+    public static void printBoard(int[][]tabBlanc, int[][] tabNoir, char[] pBlanc, char[] pNoir){
         String [][] plateau = new String [8][8];
         String type;
         System.out.println();
         for(int i =0; i<tabBlanc.length; i++){
 
             if(tabBlanc[i][0] >=0 || tabBlanc[i][1] >=0){
-                type = typePiece(i, 0);//stocke le type de la piece
+                type = typePiece(i, 0, pBlanc);//stocke le type de la piece
                 plateau[tabBlanc[i][0]][tabBlanc[i][1]] = type;// met dans la case correspondante aux positions de la pièce (non negative) le type de la pièce
             }
 
             if(tabNoir[i][0] >=0 || tabNoir[i][1] >=0){
-                type = typePiece(i, 1);//stocke le type de la piece
+                type = typePiece(i, 1, pNoir);//stocke le type de la piece
                 plateau[tabNoir[i][0]][tabNoir[i][1]] = type;// met dans la case correspondante aux positions de la pièce (non negative) le type de la pièce
             }
         }
@@ -41,37 +41,37 @@ public class Affichage {
     }
 
 
-    public static String typePiece(int indice, int couleur){
+    public static String typePiece(int indicePiece, int couleur, char []promotion){
         String chara;
         int code=0;
 
         if(couleur == 1) {
-            if (indice == 0) code = 0x2654;//roi noir
+            if (indicePiece == 0) code = 0x2654;//roi noir
 
 
-            else if (indice == 1) code = 0x2655;//reine noir
+            else if (indicePiece == 1 || (indicePiece>7 && promotion[indicePiece-7] == 68)) code = 0x2655;//reine noir
 
-            else if (indice == 2 || indice == 3) code = 0x2657;//fou noir
+            else if (indicePiece == 2 || indicePiece == 3 || (indicePiece>7 && promotion[indicePiece-7] == 70)) code = 0x2657;//fou noir
 
-            else if (indice == 4 || indice == 5) code = 0x2658;//cavalier noir
+            else if (indicePiece == 4 || indicePiece == 5 || (indicePiece>7 && promotion[indicePiece-7] == 67)) code = 0x2658;//cavalier noir
 
-            else if (indice == 6 || indice == 7) code = 0x2656;//tour noir
+            else if (indicePiece == 6 || indicePiece == 7 || (indicePiece>7 && promotion[indicePiece-7] == 84)) code = 0x2656;//tour noir
 
-            else if (indice > 7) code = 0x2659;//pion noir
+            else if (indicePiece > 7 ) code = 0x2659;//pion noir
 
         }else{
-            if (indice == 0) code = 0x265A;
+            if (indicePiece == 0) code = 0x265A;
 
 
-            else if (indice == 1) code = 0x265B;
+            else if (indicePiece == 1 || (indicePiece>7 && promotion[indicePiece-7] == 68)) code = 0x265B;
 
-            else if (indice == 2 || indice == 3) code = 0x265D;
+            else if (indicePiece == 2 || indicePiece == 3 || (indicePiece>7 && promotion[indicePiece-7] == 70)) code = 0x265D;
 
-            else if (indice == 4 || indice == 5) code = 0x265E;
+            else if (indicePiece == 4 || indicePiece == 5 || (indicePiece>7 && promotion[indicePiece-7] == 67)) code = 0x265E;
 
-            else if (indice == 6 || indice == 7) code = 0x265C;
+            else if (indicePiece == 6 || indicePiece == 7 || (indicePiece>7 && promotion[indicePiece-7] == 84)) code = 0x265C;
 
-            else if (indice > 7) code = 0x265F;
+            else if (indicePiece > 7 ) code = 0x265F;
         }
 
         chara = Character.toString((char)code);
