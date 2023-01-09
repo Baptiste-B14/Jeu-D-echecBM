f44e8b4c12e648c51d470959428ad68b9a8d0006
Baptiste-B14 <97801100+Baptiste-B14@users.noreply.github.com>
2023-01-09 01:07:00 +0100

    Update Menu.java

--- VersionFinale/Menu.java
+++ VersionFinale/Menu.java
@@ -4,18 +4,18 @@ public class Menu {
 
         //Declaration et initialisation des tableaux de positionnement
         // tableau = {{Roi},{Dame},{Fou},{Fou},{Cavalier},{Cavalier},{Tour},{Tour},{Pion},{Pion},{Pion},{Pion},{Pion},{Pion},{Pion},{Pion}};
-        //int[][] tblanc = {{0,4},{0,3},{0,2},{0,5},{0,1},{0,6},{0,0},{0,7},{1,0},{1,1},{1,2},{1,3},{1,4},{1,5},{1,6},{1,7}};
-        //int[][] tnoir = {{7,4},{7,3},{7,2},{7,5},{7,1},{7,6},{7,0},{7,7},{6,0},{6,1},{6,2},{6,3},{6,4},{6,5},{6,6},{6,7}};
+        int[][] tblanc = {{0,4},{0,3},{0,2},{0,5},{0,1},{0,6},{0,0},{0,7},{1,0},{1,1},{1,2},{1,3},{1,4},{1,5},{1,6},{1,7}};
+        int[][] tnoir = {{7,4},{7,3},{7,2},{7,5},{7,1},{7,6},{7,0},{7,7},{6,0},{6,1},{6,2},{6,3},{6,4},{6,5},{6,6},{6,7}};
 
-                                 //1 3                                                                      //pion en dame
-        //int[][] tblanc = {{1,7},{-6,-3},{-2,-5},{-1,-5},{-7,-4},{-1,-6},{-1,-1},{-3,-1},{-3,-3},{-1,-1},{-1,-2},{6,3},{-1,-4},{-1,-5},{-1,-6},{-1,-7}};
-        //int[][] tnoir = {{5,1},{-7,-3},{-7,-2},{-7,-5},{-7,-1},{-7,-6},{-7,-1},{-7,-7},{-6, -1},{-6,-1},{-6,-2},{2,4},{-6,-4},{-6,-5},{-6,-6},{-6,-7}};
+
 
         char[] pNoir =  {'p', 'p', 'p', 'p', 'p', 'p', 'p' ,'p'};
         char[] pBlanc =  {'p', 'p', 'p', 'p', 'p', 'p', 'p' ,'p'};
 
-        int[][] tblanc = {{5,2},{-1,-1},{-1,-1},{-1,-1},{-1,-1},{-1,-1},{-1,-1},{-1,-1},{-1,-1},{1,5},{2, 6},{-1,-1},{-1,-1},{-1,-1},{-1,-1},{-1,-1}};
-        int[][] tnoir = {{7,0},{0,1},{-1,-1},{-1,-1},{-1,-1},{-1,-1},{-1,-1},{-1,-1},{4,3},{-1,-1},{-1,-1},{-1,-1},{-1,-1},{-1,-1},{-1,-1},{-1,-1}};
+
+        //test de déplacement de pion, avec prise de pièce ennemie
+        //int[][] tblanc = {{5,2},{-1,-1},{-1,-1},{-1,-1},{-1,-1},{-1,-1},{-1,-1},{-1,-1},{-1,-1},{1,5},{2, 6},{-1,-1},{-1,-1},{-1,-1},{-1,-1},{-1,-1}};
+        //int[][] tnoir = {{7,0},{0,1},{-1,-1},{-1,-1},{-1,-1},{3,5},{-1,-1},{-1,-1},{4,3},{-1,-1},{-1,-1},{-1,-1},{-1,-1},{-1,-1},{-1,-1},{-1,-1}};
 
         //int[][] tblanc = {{0,7},{-1,-3},{2,5},{-1,-5},{7,4},{-1,-6},{0,1},{3,0},{3,3},{-1,-1},{-1,-2},{-1,-3},{-1,-4},{-1,-5},{-1,-6},{-1,-7}};
         //int[][] tnoir = {{5,1},{-7,-3},{-7,-2},{-7,-5},{-7,-1},{-7,-6},{-7,-1},{-7,-7},{-6, -1},{-6,-1},{-6,-2},{-6,-3},{-6,-4},{-6,-5},{-6,-6},{-6,-7}};
@@ -47,8 +47,10 @@ public class Menu {
         int tour = 0;
         int[][] tabJoueur, tabEnnemi;
         char[] promoJoueur, promoEnnemi;
-        long chronoBlanc;
-        long chronoNoir;
+        long chronoBlanc =0;
+        long chronoNoir = 0;
+        long chronoJoueur = 0;
+        boolean resultFonction;
 
         int indicePiece;
         int[] position = new int[2];
@@ -56,6 +58,17 @@ public class Menu {
         Affichage.printBoard(tblanc, tnoir, pBlanc, pNoir);
 
         do {
+            if(blitz){
+                if(tour==0 || tour == 1){
+                    chronoJoueur = Chrono.start();
+                }
+                else{
+                    if(tour%2 ==0) chronoJoueur = Chrono.resume(chronoBlanc);
+                    else chronoJoueur = Chrono.resume(chronoNoir);
+                }
+
+            }
+
             int[][] tabSolutions = {{-1,-1}, {-1,-1}, {-1,-1}, {-1,-1}, {-1,-1}};
             couleur = tour % 2;
             if (couleur == 1) sCouleur = "noir";
@@ -106,19 +119,22 @@ public class Menu {
 
                             //Pas d'echec et mat (testé à la fin du tour précédent. Comme la position demandée ne met pas en echec
                             //on peut vérifier que la position est correcte.
-                            if(!Fonction.caseDisponible(position[0], position[1], tabJoueur))
+                            resultFonction = Fonction.caseDisponible(position[0], position[1], tabJoueur);
+                            if(!resultFonction)
                                 System.out.println("La case est déjà occupée par une de vos pièces");
-                        } while (!Fonction.caseDisponible(position[0], position[1], tabJoueur));
-
-                        if(!Fonction.verificationDeplacementPossible(indicePiece, position[0], position[1], couleur, tabJoueur, tabEnnemi, promoJoueur))
-                            System.out.println("Le déplacement ne correspond pas à la pièce choisit");
-                    } while (!Fonction.verificationDeplacementPossible(indicePiece, position[0], position[1], couleur, tabJoueur, tabEnnemi, promoJoueur));
-                    if(!Fonction.verificationDuChemin(indicePiece, position[0], position[1], tabJoueur, tabEnnemi, promoJoueur))
+                        } while (!resultFonction);
+                        resultFonction = Fonction.verificationDeplacementPossible(indicePiece, position[0], position[1], couleur, tabJoueur, tabEnnemi, promoJoueur);
+                        if(!resultFonction)
+                            System.out.println("Le déplacement ne correspond pas à la pièce choisie");
+                    } while (!resultFonction);
+                    resultFonction = Fonction.verificationDuChemin(indicePiece, position[0], position[1], tabJoueur, tabEnnemi, promoJoueur);
+                    if(!resultFonction)
                         System.out.println("Une pièce se trouve sur le chemin");
-                } while (!Fonction.verificationDuChemin(indicePiece, position[0], position[1], tabJoueur, tabEnnemi, promoJoueur));
+                } while (!resultFonction);
                 Fonction.actualisationEchiquier(indicePiece, position[0], position[1], tabJoueur, tabEnnemi);
-                if(Fonction.echecRoi(couleur, tabJoueur, tabEnnemi, promoEnnemi)) System.out.println("Votre roi sera en echec avec ce déplacement");
-            } while (Fonction.echecRoi(couleur, tabJoueur, tabEnnemi, promoEnnemi));//Tant que echec, alors on doit choisir une position valide
+                resultFonction = Fonction.echecRoi(couleur, tabJoueur, tabEnnemi, promoEnnemi);
+                if(resultFonction) System.out.println("Votre roi sera en echec avec ce déplacement");
+            } while (resultFonction);//Tant que echec, alors on doit choisir une position valide
 
             if(Fonction.promotionPossible(indicePiece, couleur, tabJoueur, promoJoueur))Fonction.promotion(indicePiece, promoJoueur);
 
@@ -142,8 +158,15 @@ public class Menu {
 
 
             Affichage.affichageSolutions(aide, tabSolutions);
-            tour++;
 
+
+            if(tour%2 == 0){
+                chronoBlanc = Chrono.pause(chronoJoueur);
+            }else chronoNoir = Chrono.pause(chronoJoueur);
+            System.out.println("" + chronoJoueur);
+
+            tour++;
         }while(!echecEtMat);
+
     }//menu()
 }//class menu
