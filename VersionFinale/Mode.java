fcdba58f8da018e19177b89b1e24389fba93e5bd
Baptiste-B14 <97801100+Baptiste-B14@users.noreply.github.com>
2023-01-10 15:26:00 +0100

    Update Mode.java

--- VersionFinale/Mode.java
+++ VersionFinale/Mode.java
@@ -2,32 +2,34 @@ import java.util.Scanner;
 
 public class Mode {
 
+    /**
+     * Méthode qui permet d'activer certaines fonctions du jeu.
+     * Elle utilise la saisie forcée afin de pouvoir activer ou non les fonctions. En fonction des résultats, le jeu sera créé.
+     */
     public static void mode(){
         Scanner sc = new Scanner(System.in).useDelimiter("\n");
-        boolean blitz;
+        boolean blitz = false;
         String choixBlitz;
         boolean aide=false;
         String reponse;
         System.out.println("Si vous êtes novices, une aide est possible souhaitez-vous en bénéficier ? [Oui/Non]");
 
-        do{
+        do{//saisie forcée de oui ou non (la casse est ignorée grace au .toLowerCase())
             reponse = sc.nextLine().toLowerCase();
         }while(!reponse.equals("oui") && !reponse.equals("non"));
-        if (reponse.equals("oui"))
+
+        if (reponse.equals("oui"))//active l'aide aux solutions
             aide=true;
 
-        do{
+        do{//saisie forcée pour le mode blitz
             System.out.println("Mode de jeu blitz ? ( Oui/Non )");
             choixBlitz = sc.nextLine().toLowerCase();
         }while(!choixBlitz.equals("oui") && !choixBlitz.equals("non"));
 
+        if(choixBlitz.equals("oui"))blitz = true;//active le mode blitz
 
-        if(choixBlitz.equals("oui"))blitz = true;
-        else blitz = false;
-
-
-        System.out.println("Pour sélectionner une case, veuillez respecter le format LettreChiffre (B5) tout au long de la partie.");
-        Menu.menu(aide, blitz);
+        System.out.println("Pour sélectionner une case, veuillez respecter le format LettreChiffre (B5) tout au long de la partie.");//indication pour le format de saisie des cases.
+        Jeu.jeu(aide, blitz);//création du jeu
     }
 
 
